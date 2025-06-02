8b5dbd972eb95a9e42483005247a2638a329f4d9
Toshiyuki Takahagi <tosiyuki@hh.iij4u.or.jp>
2025-06-02 20:57:10 +0900

    JsonParser クラスの追加とメソッドの変更
    `ParseJSON.cpp` に `JsonParser` クラスを追加し、JSON 解析のメソッドを静的関数からインスタンスメソッドに変更しました。これにより、`m_jsonText`、`m_offset`、`m_callback` などのメンバー変数が使用され、オブジェクト指向的な設計が強化されました。また、`ParseJSON` 関数のシグネチャが変更され、コールバック関数の引数も更新されました。
    
    `ParseJSON.h` では、`NotificationType` 列挙型の値が明確化され、新しい関数が追加されて、UTF-8 と UTF-16 の変換や JSON 文字列のアンエスケープ処理が強化されました。

--- SimpleJsonParser/ParseJSON.h
+++ SimpleJsonParser/ParseJSON.h
@@ -1,3 +1,8 @@
+// 簡易JSONパーサー
+
+//---------------
+//	Include Files
+//---------------
 #include <string_view>
 #include <string>
 #include <map>
@@ -6,35 +11,75 @@
 #include <functional>
 
 #include <ppl.h>
+
 namespace Morrin::JSON
 {
+
+//---------------
+//	defines
+//---------------
 enum class NotificationType
 {
 	StartParse,
 	EndParse,
 
-	StartElement,
-	EndElement,
-
-	StartObject,
-	EndObject,
+	StartObject = '{',
+	EndObject = '}',
 	
-	StartArray,
-	EndArray,
+	StartArray = '[',
+	EndArray = ']',
 
-	Key,
-	String,
-	Number,
-	BooleanTrue,
-	BooleanFalse,
-	Null,
+	Key = ':',
+	String = '"',
+	Number = '0',
+	BooleanTrue = 't',
+	BooleanFalse = 'f',
+	Null = 'n',
 };
+
+
+
 using ParseCallBack = std::function<bool( NotificationType type, const std::string_view& value )> const;
 
+using JsonRefKeyType = std::string_view;	// キーは UTF8 のまま
 using JsonKeyType = std::string;	// キーは UTF8 のまま
 using JsonObject = std::map<JsonKeyType, std::any>;
 using JsonArray = std::vector<std::any>;	// 配列は std::any で値を保持する
+using JsonString = std::wstring;	// 文字列データは、正規化しておく
+//---------------
+//	prottype
+//---------------
+bool __stdcall ParseJSON( const std::string_view& jsonText, const ParseCallBack& proc );
 
-bool __stdcall ParseJSON( const std::string_view& jsonText, ParseCallBack&& proc );
+// 構造化データとしてパースするデータは、永続化可能とする。
 std::any __stdcall ParseJSON( const std::string_view& jsonText, const concurrency::cancellation_token& token = concurrency::cancellation_token::none() );
+std::any APIENTRY GetValue( std::any obj, const JsonRefKeyType& searchKey );
+
+//	string_view が参照しているテキストをアンエスケープする
+std::string __stdcall UnEscapeToString( const std::string_view& value );
+std::wstring __stdcall UnEscapeToWstring( const std::string_view& value );
+
+// JsonObject では、数値は int のみとする
+int __stdcall ConvertToInt( const std::string_view& value );
+
+// UTF-8 への変換
+std::string __stdcall ToString( const std::wstring_view& str, UINT codePage );
+inline std::string __stdcall ToString( LPCWSTR str, UINT codePage )
+{
+	return ToString( std::wstring_view( str ), codePage );
+}
+inline std::string __stdcall ToUtf8( const std::wstring_view& str )
+{
+	return ToString( str, CP_UTF8 );
+}
+inline std::string ToUtf8( LPCWSTR str )
+{
+	return ToString( std::wstring_view( str ), CP_UTF8 );
+}
+std::wstring __stdcall ToWString( const std::string_view& u8str, UINT codePage = CP_UTF8 );
+inline std::wstring ToWString( LPCSTR str, UINT codePage = CP_UTF8 )
+{
+	return ToWString( std::string_view( str ), codePage );
+}
+
 }
