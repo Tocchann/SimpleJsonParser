1c32b8bc0639c86728becc3b940b9980737c7d72
Toshiyuki Takahagi <tosiyuki@hh.iij4u.or.jp>
2025-06-02 20:57:36 +0900

    SimpleJsonParser.cpp のインクルードとパース処理を更新
    `#include <windows.h>` を削除し、`#include "stdafx.h"` を追加しました。
    `LoadFile` 関数でのファイルパス変換を `CW2AEX` から `Morrin::JSON::ToString` に変更しました。
    `wmain` 関数にラムダ関数を追加し、JSON パースの通知タイプに基づくメッセージ出力を実装しました。
    最初のキー名を取得するための変数 `firstKeyName` を導入し、デバッグ用のアサーションを追加してパース結果の検証を強化しました。

--- SimpleJsonParser/SimpleJsonParser.cpp
+++ SimpleJsonParser/SimpleJsonParser.cpp
@@ -1,7 +1,7 @@
 ﻿// SimpleJsonParser.cpp : このファイルには 'main' 関数が含まれています。プログラム実行の開始と終了がそこで行われます。
 //
 
-#include <windows.h>
+#include "stdafx.h"
 #include <stdio.h>
 
 #include "ParseJSON.h"
@@ -11,15 +11,14 @@
 
 #include <iostream>
 #include <format>
-
+#include <sstream>
 
 
 static std::string LoadFile( const wchar_t* filePath )
 {
 	CAtlFile file;
 	HRESULT hr = file.Create( filePath, GENERIC_READ, FILE_SHARE_READ, OPEN_EXISTING );
-	CW2AEX<MAX_PATH> cw2a( filePath, CP_ACP );
-	LPCSTR filePathA = cw2a;
+	std::string filePathA = Morrin::JSON::ToString( filePath, CP_ACP );
 	if( FAILED( hr ) )
 	{
 		std::wcerr << std::format( L"Failed to open file: {} (HRESULT: 0x{:08X})\n", filePath, hr );
@@ -56,26 +55,62 @@ int wmain( int argc, wchar_t* argv[] )
 	{
 		std::string jsonText = LoadFile( argv[index] );
 
-		auto result = Morrin::JSON::ParseJSON( jsonText, []( const Morrin::JSON::NotificationType& type, const std::string_view& value ) -> bool
+		Morrin::JSON::JsonKeyType firstKeyName;
+		// 簡易パーサーでストレートな変換を試す(テストコードなのでこのまま展開出来ていればよい)
+		std::wcout << L"Direct Parse of JSON:" << std::endl;
+		auto result = Morrin::JSON::ParseJSON( jsonText, [&firstKeyName]( const Morrin::JSON::NotificationType& type, const std::string_view& value ) -> bool
 		{
+			std::wstring typeText;
+			switch( type )
+			{
+			case Morrin::JSON::NotificationType::StartParse:	typeText = L"StartParse"; break;
+			case Morrin::JSON::NotificationType::EndParse:		typeText = L"EndParse"; break;
+			case Morrin::JSON::NotificationType::StartObject:	typeText = L"StartObject"; break;
+			case Morrin::JSON::NotificationType::EndObject:		typeText = L"EndObject"; break;
+			case Morrin::JSON::NotificationType::StartArray:	typeText = L"StartArray"; break;
+			case Morrin::JSON::NotificationType::EndArray:		typeText = L"EndArray"; break;
+			case Morrin::JSON::NotificationType::Key:			typeText = L"Key"; break;
+			case Morrin::JSON::NotificationType::String:		typeText = L"String"; break;
+			case Morrin::JSON::NotificationType::Number:		typeText = L"Number"; break;
+			case Morrin::JSON::NotificationType::BooleanTrue:	typeText = L"BooleanTrue"; break;
+			case Morrin::JSON::NotificationType::BooleanFalse:	typeText = L"BooleanFalse"; break;
+			case Morrin::JSON::NotificationType::Null:			typeText = L"Null"; break;
+			default:											typeText = std::format( L"Unknown NotificationType: {}", static_cast<int>(type) );	break;
+			}
+			std::wstring msg;
+			if( type == Morrin::JSON::NotificationType::StartParse )
+			{
+				msg = std::format( L"Value:`{}`,\t\tNotificationType::{}, \n", Morrin::JSON::ToWString( value ), typeText );
+			}
+			else
+			{
+				msg = std::format( L"Value:`{}`,\t\tNotificationType::{}, \n", Morrin::JSON::UnEscapeToWstring( value ), typeText );
+			}
+			if( firstKeyName.empty() && type == Morrin::JSON::NotificationType::Key )
+			{
+				firstKeyName = Morrin::JSON::UnEscapeToString( value );
+			}
+			std::wcout << msg;
+			OutputDebugString( msg.c_str() );
 			// ここをトレースするだけでよい
 			return true; // trueを返すと、さらに子要素のパースを続ける
 		} );
+		_ASSERTE( result );
 		if( !result )
 		{
 			break;
 		}
+		// こっちは純粋にデバッグ実行程度しかしないユニットテストにしてないのでまぁ面倒ごとはいらないでしょうｗ
+		auto obj = Morrin::JSON::ParseJSON( jsonText );
+		_ASSERTE( obj.has_value() );
+		switch( jsonText[0] )
+		{
+		case '{':	_ASSERTE( obj.type() == typeid(Morrin::JSON::JsonObject) );	break;
+		case '[':	_ASSERTE( obj.type() == typeid(Morrin::JSON::JsonArray) );	break;
+		}
+		// 何が来てるかわからないけど最初のオブジェクトのキーを取得してみる
+		auto firstObj = Morrin::JSON::GetValue( obj, firstKeyName );
+		_ASSERTE( firstObj.has_value() && firstObj.type() == typeid(Morrin::JSON::JsonObject) );
 	}
 	return 0;
 }
-
-// プログラムの実行: Ctrl + F5 または [デバッグ] > [デバッグなしで開始] メニュー
-// プログラムのデバッグ: F5 または [デバッグ] > [デバッグの開始] メニュー
-
-// 作業を開始するためのヒント: 
-//    1. ソリューション エクスプローラー ウィンドウを使用してファイルを追加/管理します 
-//   2. チーム エクスプローラー ウィンドウを使用してソース管理に接続します
-//   3. 出力ウィンドウを使用して、ビルド出力とその他のメッセージを表示します
-//   4. エラー一覧ウィンドウを使用してエラーを表示します
-//   5. [プロジェクト] > [新しい項目の追加] と移動して新しいコード ファイルを作成するか、[プロジェクト] > [既存の項目の追加] と移動して既存のコード ファイルをプロジェクトに追加します
-//   6. 後ほどこのプロジェクトを再び開く場合、[ファイル] > [開く] > [プロジェクト] と移動して .sln ファイルを選択します
